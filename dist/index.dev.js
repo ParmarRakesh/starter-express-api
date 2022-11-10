1e2b7af0091bcc3af7ba4e98ce2bf744f861818a
rparmar2007 <rparmar2007@gmail.com>
2022-11-10 17:04:38 +0530

    remove promise

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -58,8 +58,8 @@ function get_stock(product_id) {
   });
 }
 
-function decrease_stock(product_id) {
-  return regeneratorRuntime.async(function decrease_stock$(_context4) {
+function decrease_stock_promise(product_id) {
+  return regeneratorRuntime.async(function decrease_stock_promise$(_context4) {
     while (1) {
       switch (_context4.prev = _context4.next) {
         case 0:
@@ -111,8 +111,8 @@ function decrease_stock(product_id) {
   });
 }
 
-function increase_stock(product_id) {
-  return regeneratorRuntime.async(function increase_stock$(_context6) {
+function increase_stock_promise(product_id) {
+  return regeneratorRuntime.async(function increase_stock_promise$(_context6) {
     while (1) {
       switch (_context6.prev = _context6.next) {
         case 0:
@@ -164,11 +164,91 @@ function increase_stock(product_id) {
   });
 }
 
-app.post("/increase", function _callee4(request, response) {
-  var json, id, increased_stock;
-  return regeneratorRuntime.async(function _callee4$(_context7) {
+function decrease_stock(product_id) {
+  var current_stock, data, config;
+  return regeneratorRuntime.async(function decrease_stock$(_context7) {
     while (1) {
       switch (_context7.prev = _context7.next) {
+        case 0:
+          _context7.next = 2;
+          return regeneratorRuntime.awrap(get_stock(product_id));
+
+        case 2:
+          current_stock = _context7.sent;
+          current_stock = current_stock - 1;
+          data = JSON.stringify({
+            stock_quantity: current_stock
+          });
+          config = {
+            method: "put",
+            url: "https://alittlething.co/wp-json/wc/v3/products/".concat(product_id),
+            headers: {
+              Authorization: "Basic Y2tfYjc2MjZmYWJhM2MzYTFkNWU0ZjMyYmNhMmQ5NGFjMTM1NWUzMjE1Mjpjc18yNGM0ZGU5ZDU1Y2Y0ZjExYTRiMmU3MWQ1NGVlOTJhMGE0MGExNzFm",
+              "Content-Type": "application/json",
+              Cookie: "PHPSESSID=ctdi4iuc2l05sqekepdm5cipqo; mailchimp_landing_site=https%3A%2F%2Falittlething.co%2Fwp-json%2Fwc%2Fv3%2Fproducts%2F20186; wfwaf-authcookie-65e717bb942274366e37ef93fe37e38d=827%7Cadministrator%7Cmanage_options%2Cunfiltered_html%2Cedit_others_posts%2Cupload_files%2Cpublish_posts%2Cedit_posts%2Cread%7C7f126310333728ebad0db9da48b967fded175b927eabb592c4f3bc8d7e2d2ab3"
+            },
+            data: data
+          };
+          axios(config).then(function (response) {
+            //console.log(response.data);
+            return response.data.stock_quantity;
+          })["catch"](function (error) {
+            console.log(error);
+          });
+
+        case 7:
+        case "end":
+          return _context7.stop();
+      }
+    }
+  });
+}
+
+function increase_stock(product_id) {
+  var current_stock, data, config;
+  return regeneratorRuntime.async(function increase_stock$(_context8) {
+    while (1) {
+      switch (_context8.prev = _context8.next) {
+        case 0:
+          _context8.next = 2;
+          return regeneratorRuntime.awrap(get_stock(product_id));
+
+        case 2:
+          current_stock = _context8.sent;
+          current_stock = current_stock + 1;
+          data = JSON.stringify({
+            stock_quantity: current_stock
+          });
+          config = {
+            method: "put",
+            url: "https://alittlething.co/wp-json/wc/v3/products/".concat(product_id),
+            headers: {
+              Authorization: "Basic Y2tfYjc2MjZmYWJhM2MzYTFkNWU0ZjMyYmNhMmQ5NGFjMTM1NWUzMjE1Mjpjc18yNGM0ZGU5ZDU1Y2Y0ZjExYTRiMmU3MWQ1NGVlOTJhMGE0MGExNzFm",
+              "Content-Type": "application/json",
+              Cookie: "PHPSESSID=ctdi4iuc2l05sqekepdm5cipqo; mailchimp_landing_site=https%3A%2F%2Falittlething.co%2Fwp-json%2Fwc%2Fv3%2Fproducts%2F20186; wfwaf-authcookie-65e717bb942274366e37ef93fe37e38d=827%7Cadministrator%7Cmanage_options%2Cunfiltered_html%2Cedit_others_posts%2Cupload_files%2Cpublish_posts%2Cedit_posts%2Cread%7C7f126310333728ebad0db9da48b967fded175b927eabb592c4f3bc8d7e2d2ab3"
+            },
+            data: data
+          };
+          axios(config).then(function (response) {
+            console.log(response.data.stock_quantity);
+            return response.data.stock_quantity;
+          })["catch"](function (error) {
+            console.log(error);
+          });
+
+        case 7:
+        case "end":
+          return _context8.stop();
+      }
+    }
+  });
+}
+
+app.post("/increase", function _callee4(request, response) {
+  var json, id, increased_stock;
+  return regeneratorRuntime.async(function _callee4$(_context9) {
+    while (1) {
+      switch (_context9.prev = _context9.next) {
         case 0:
           json = {
             key: "hello"
@@ -177,30 +257,30 @@ app.post("/increase", function _callee4(request, response) {
           console.log("test");
           id = request.body.id; // const decreased_stock = await decrease_stock(current_stock, id);
 
-          _context7.next = 6;
+          _context9.next = 6;
           return regeneratorRuntime.awrap(increase_stock(id));
 
         case 6:
-          increased_stock = _context7.sent;
+          increased_stock = _context9.sent;
           console.log("\n    sku_id: ".concat(id, ",\n    increased_stock: ").concat(increased_stock, ",\n  ")); // response.send({
           //   sku_id: id,
           //   increased_stock: increased_stock,
           // });
 
-          return _context7.abrupt("return", response.status(200));
+          return _context9.abrupt("return", response.status(200));
 
         case 9:
         case "end":
-          return _context7.stop();
+          return _context9.stop();
       }
     }
   });
 });
 app.post("/decrease", function _callee5(request, response) {
   var json, id, decreased_stock;
-  return regeneratorRuntime.async(function _callee5$(_context8) {
+  return regeneratorRuntime.async(function _callee5$(_context10) {
     while (1) {
-      switch (_context8.prev = _context8.next) {
+      switch (_context10.prev = _context10.next) {
         case 0:
           json = {
             key: "hello"
@@ -208,20 +288,20 @@ app.post("/decrease", function _callee5(request, response) {
           console.log(request.body);
           console.log("test");
           id = request.body.id;
-          _context8.next = 6;
+          _context10.next = 6;
           return regeneratorRuntime.awrap(decrease_stock(id));
 
         case 6:
-          decreased_stock = _context8.sent;
+          decreased_stock = _context10.sent;
           console.log("Call to decrease for id ".concat(id)); //const increased_stock = await increase_stock(current_stock, id);
           //console.log("increased stock:", increased_stock);
 
           console.log("\n    sku_id: ".concat(id, ",\n    decreased_stock: ").concat(decreased_stock, ",\n  "));
-          return _context8.abrupt("return", response.status(200));
+          return _context10.abrupt("return", response.status(200));
 
         case 10:
         case "end":
-          return _context8.stop();
+          return _context10.stop();
       }
     }
   });
