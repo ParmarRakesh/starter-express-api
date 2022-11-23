978bd5c461693de89f7385c3249bc42f4e0133b8
rparmar2007 <rparmar2007@gmail.com>
2022-11-23 14:31:25 +0530

    https

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var axios = require("axios");
+const https = require("https");
 
 const app = express();
 // For parsing application/json
@@ -120,7 +121,6 @@ async function decrease_stock(product_id) {
       Cookie:
         "PHPSESSID=ctdi4iuc2l05sqekepdm5cipqo; mailchimp_landing_site=https%3A%2F%2Falittlething.co%2Fwp-json%2Fwc%2Fv3%2Fproducts%2F20186; wfwaf-authcookie-65e717bb942274366e37ef93fe37e38d=827%7Cadministrator%7Cmanage_options%2Cunfiltered_html%2Cedit_others_posts%2Cupload_files%2Cpublish_posts%2Cedit_posts%2Cread%7C7f126310333728ebad0db9da48b967fded175b927eabb592c4f3bc8d7e2d2ab3",
     },
-    httpAgent: new http.Agent({ keepAlive: true }),
     httpsAgent: new https.Agent({ keepAlive: true }),
     data: data,
   };
@@ -155,7 +155,6 @@ async function increase_stock(product_id) {
       Cookie:
         "PHPSESSID=ctdi4iuc2l05sqekepdm5cipqo; mailchimp_landing_site=https%3A%2F%2Falittlething.co%2Fwp-json%2Fwc%2Fv3%2Fproducts%2F20186; wfwaf-authcookie-65e717bb942274366e37ef93fe37e38d=827%7Cadministrator%7Cmanage_options%2Cunfiltered_html%2Cedit_others_posts%2Cupload_files%2Cpublish_posts%2Cedit_posts%2Cread%7C7f126310333728ebad0db9da48b967fded175b927eabb592c4f3bc8d7e2d2ab3",
     },
-    httpAgent: new http.Agent({ keepAlive: true }),
     httpsAgent: new https.Agent({ keepAlive: true }),
     data: data,
   };
