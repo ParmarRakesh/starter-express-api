4a301a24448fab97ea9fa37af4fcf3b9677a74e0
rparmar2007 <rparmar2007@gmail.com>
2023-03-09 11:13:56 +0530

    removed package

--- dist/config.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var authtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiUmFrZXNoIiwibmFtZSI6Ikludm9pY2UgdG8gQnViYmxlIiwiQVBJX1RJTUUiOjE2Njg2NDkwMTR9.U73f7G8swnTIKOrkSQ9ZvpmX0hBFdo6X9N1FYLBtTeI";
-var cookie = "sp_session=c1cnl84rjevlttki0ar76n9urt11hbog";
-module.exports = {
-  authtoken: authtoken,
-  cookie: cookie
-};
